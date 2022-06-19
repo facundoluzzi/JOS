fc64d0efd156cebaf9b60687ef883787e0fdbda5
argelignacio <iargel@fi.uba.ar>
2022-06-18 22:51:30 -0300

    mem_init_mp

--- kern/pmap.c
+++ kern/pmap.c
@@ -297,6 +297,14 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
+	for (int i = 0; i <= NCPU - 1; i++) {
+		uintptr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		boot_map_region(kern_pgdir,
+		                kstacktop_i - KSTKSIZE,
+		                KSTKSIZE,
+		                PADDR(percpu_kstacks[i]),
+		                PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
